7bf6391ac15c3b08232d9a72fb8f9b443c827753
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-03-29 10:16:30 -0700

    Add function to create anchor for trumps.

--- Trump Smash/Scene.swift
+++ Trump Smash/Scene.swift
@@ -38,6 +38,39 @@ class Scene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if currentTime > creationTime {
+            createTrumpAnchor()
+            creationTime = currentTime + TimeInterval(randomFloat(min: 3.0, max: 6.0))
+        }
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
+    func createTrumpAnchor() {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        // Define 360 degrees in radians
+        let _360degrees = 2.0 * Float.pi
+        //Place Trump in random position. Create random rotation matrix on the X and Y axis.
+        let rotateX = simd_float4x4(SCNMatrix4MakeRotation(_360degrees * randomFloat(min: 0.0, max: 1.0), 1, 0, 0))
+        //SCNMatrix4MakeRotation returns a matrix describing a rotation transformation.
+        // simd_float4x4 coverts result into a 4x4 matrix
+        let rotateY = simd_float4x4(SCNMatrix4MakeRotation(_360degrees * randomFloat(min: 0.0, max: 1.0), 0, 1, 0))
+        // Combine both rotation matrices with a multiplication operation
+        let rotation = simd_mul(rotateX, rotateY)
+        // Create a translation matrix in the Z-axis with a random value between -1 and -2 meters
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1 - randomFloat(min: 0.0, max: 1.0)
+        // Combine the rotation and translation matrices
+        let transform = simd_mul(rotation, translation)
+        // Create and add anchor to the screen
+        let anchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: anchor)
+        // Increment the Trump counter
+        trumpCount += 1
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
